0e467b03d622171058233ef117518a09e94dd8ed
Mark Babirad <mbabirad@brainstation.io>
2024-05-08 14:47:03 -0700

    add function examples

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,34 @@
 console.log("Hello JavaScript FUN-ctions");
+
+function greetWorld() {
+  console.log("Hello, world!");
+  console.log("Bonjour, monde");
+  console.log("¡Hola, mundo!");
+  console.log("Hallo, Welt!");
+  console.log("你好，世界！");
+  console.log("Привет, мир!");
+}
+
+greetWorld();
+greetWorld();
+console.clear();
+
+function greetPerson(person) {
+  console.log("Hello " + person);
+}
+
+greetPerson("Umer");
+greetPerson("Mark");
+greetPerson();
+console.clear();
+
+function addTwoNumbers(num1, num2) {
+  //   console.log("num1: ", num1);
+  //   console.log("num2: ", num2);
+  //   console.log("num3: ", num3);
+
+  let sum = num1 + num2;
+  console.log(sum);
+}
+
+addTwoNumbers(10, 20);
